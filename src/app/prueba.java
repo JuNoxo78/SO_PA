85dca8aff1c1d1db009e5d62ad4515980c8edd5e
Juan López <juanandrelopezcordova8@gmail.com>
2024-12-04 03:34:11 +0000

    change files, add bd folder

---
+++ src/app/prueba.java
@@ -0,0 +1,9 @@
+package app;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo.");
+	}
+	
+}
--- src/interfaces/main.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public class main {
-
-	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-	}
-	
-}
